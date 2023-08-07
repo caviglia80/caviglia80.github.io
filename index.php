ee85f9d249bc6be8ece0195b81b8941d414a179a
Fran <caviglia80@gmail.com>
2023-08-07 14:59:32 -0300

    .

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-$dominio = $_SERVER['HTTP_HOST'];
-
-if (strpos($dominio, 'caviglia80') !== false) {
-    header('Location: dist/caviglia80.github.io/');
-    exit;
-} else {
-    echo "Dominio desconocido";
-    exit;
-}
+header('Location: dist/caviglia80.github.io/');
+exit;
 
