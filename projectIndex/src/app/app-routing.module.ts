7f1f7aef199cf83760952a915de81f9a8d72b3a1
Fran <caviglia80@gmail.com>
2023-08-07 22:16:43 -0300

    .

--- projectIndex/src/app/app-routing.module.ts
+++ projectIndex/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  { path: 'main', component: HomeComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: '**', redirectTo: '/main', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
