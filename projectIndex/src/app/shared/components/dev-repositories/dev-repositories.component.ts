c10ce09104d06f3838faca0a594b888b7073151f
Fran <caviglia80@gmail.com>
2023-08-27 20:59:08 -0300

    .

--- projectIndex/src/app/shared/components/dev-repositories/dev-repositories.component.ts
+++ projectIndex/src/app/shared/components/dev-repositories/dev-repositories.component.ts
@@ -1,23 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GithubService } from '@services/github.service';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dev-repositories',
   templateUrl: './dev-repositories.component.html',
   styleUrls: ['./dev-repositories.component.css']
 })
-export class DevRepositoriesComponent {
+export class DevRepositoriesComponent implements OnInit {
   public repos$: Observable<any[]> = this.githubService.getRepos();
 
-  constructor(private githubService: GithubService) { }
+  constructor(public githubService: GithubService) { }
+
+  ngOnInit() {
+     this.asd('EstudioJuridico');
+  }
 
   public truncateDescription(description: string): string {
     try {
       if (description)
-      return description.length > 80 ? description.substring(0, 80) + '..' : description;
-    else
-      return '';
+        return description.length > 80 ? description.substring(0, 80) + '..' : description;
+      else
+        return '';
     } catch (error) {
       console.log(error);
       return '';
@@ -27,22 +32,47 @@ export class DevRepositoriesComponent {
   public truncateTitle(title: string): string {
     try {
       if (title)
-      return title.length > 20 ? title.substring(0, 20) + '..' : title;
-    else
-      return '';
+        return title.length > 20 ? title.substring(0, 20) + '..' : title;
+      else
+        return '';
     } catch (error) {
       console.log(error);
       return '';
     }
   }
 
-  public languageList(repoName: string):Observable<any[]> {
-    try {
-      return this.githubService.getLanguages(repoName);
-    } catch (error) {
-      console.log(error);
-      return new Observable<any[]>;
+  /*   public languageList(repoName: string): void {
+      this.githubService.getLanguages(repoName)
+        .subscribe(
+          response => {
+            console.log('Language Data:', response);
+          },
+          error => {
+            console.error('Error:', error);
+          }
+        );
+    } */
+
+
+    public asd(repoName: string) {
+      return this.githubService.getLanguages(repoName).pipe(
+        map((languages: string[]) => {
+
+          console.log(languages);
+
+        })
+      );
     }
-  }
+
+    public getLanguagesData(repoName: string): Observable<string[]> {
+      return this.githubService.getLanguages(repoName).pipe(
+        map((languages: string[]) => {
+
+          console.log(languages);
+
+          return languages;
+        })
+      );
+    }
 
 }
--- projectIndex/src/app/shared/services/github.service.ts
+++ projectIndex/src/app/shared/services/github.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +10,21 @@ export class GithubService {
   constructor(private http: HttpClient) { }
 
   public getRepos(): Observable<any[]> {
-    try {
-      return this.http.get<any[]>(`https://api.github.com/users/caviglia80/repos`);
-    } catch (error) {
-      console.log(error);
-      return new Observable<any[]>;
-    }
+    return this.http.get<any[]>('https://api.github.com/users/caviglia80/repos')
+      .pipe(
+        catchError(error => {
+          console.error(error);
+          return this.handleError('An error occurred while fetching repositories.');
+        })
+      );
   }
 
-  public getLanguages(repoName: string): Observable<any[]> {
-    try {
-      return this.http.get<any[]>('https://api.github.com/repos/caviglia80/' + repoName + '/languagesrepoName');
-    } catch (error) {
-      console.log(error);
-      return new Observable<any[]>;
-    }
+  public getLanguages(repoName: string): Observable<string[]> {
+    const url = `https://api.github.com/repos/caviglia80/${repoName}/languages`;
+    return this.http.get<any>(url)
   }
 
+  private handleError(errorMessage: string): Observable<never> {
+    return throwError(errorMessage);
+  }
 }
